442b6b5b7a7dd1378dfc09fbb22019b00673fa26
Aleksandar Ljubišić <simke20@yahoo.com>
2017-10-12 17:49:33 +0200

    Show user data on the page

--- js/main.js
+++ js/main.js
@@ -11,13 +11,42 @@ $(document).ready(function(){
         $.ajax({
             url:'https://api.github.com/users/' + username, 
             // my data
-            data: {
+            data:{
                 client_id:'2ac9b390903333e799de',
                 client_secret:'d8dbb523e73b8b464b5a9f548954456fe53931e1'
             }
         }).done(function(user){
-            console.log(user);
-        });
 
+            // select div with id="profile" and put user data into
+            $('#profile').html(`
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">${user.name}</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="${user.avatar_url}" alt="No Image Found" title="${user.name}" class="thumbnail avatar">
+                            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                            <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                            <span class="label label-success">Followers: ${user.followers}</span>
+                            <span class="label label-info">Following: ${user.following}</span>
+                            <br><br>
+                            <ul class="list-group">
+                                <li class="list-group-item">Company: ${user.company}</li>
+                                <li class="list-group-item">Website/blog: <a href="${user.blog}" target="_blank">View Website</a></li>
+                                <li class="list-group-item">Location: ${user.location}<//li>
+                                <li class="list-group-item">Email: ${user.email}</li>
+                                <li class="list-group-item">Member Since: ${user.created_at}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `);
+        });
     })
 });
